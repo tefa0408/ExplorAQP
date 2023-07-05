5700b538c8725b8e93bce6b2ce71b266ac3f6786
tefa0408 <ealfaroz@unsa.edu,pe>
2023-07-05 02:48:09 -0500

    conexión de autenticación completa con firebase

--- app/src/main/java/com/example/ges_touristapp/LoginActivity.kt
+++ app/src/main/java/com/example/ges_touristapp/LoginActivity.kt
@@ -1,19 +1,47 @@
 package com.example.ges_touristapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.ges_touristapp.databinding.ActivityLoginBinding
 import com.example.ges_touristapp.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var  binding: ActivitySignupBinding
+    private lateinit var  binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginButton.setOnClickListener{
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty() ){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MapsActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Los campos no pueden estar vacios", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        binding.signupRedirectText.setOnClickListener{
+            val signupIntent = Intent(this, SignupActivity::class.java)
+            startActivity(signupIntent)
+        }
     }
 }
